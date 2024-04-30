4ea307cc91385389f1982afcc4f5050db10c11a9
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-04-30 17:41:34 +0900

    kaya

---
+++ GitTestProject/GitTestProject/Kaya/Kaya.swift
@@ -0,0 +1,18 @@
+//
+//  Kaya.swift
+//  GitTestProject
+//
+//  Created by 조세연 on 4/30/24.
+//
+
+import SwiftUI
+
+struct Kaya: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Kaya()
+}
