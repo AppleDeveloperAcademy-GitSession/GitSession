7388ccf9a1d3e3ded66da1228ea00908e0d57b47
KyungsooLee <jmtkd9196@gmail.com>
2024-04-30 17:37:05 +0900

    First Commit

---
+++ GitTestProject/GitTestProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTestProject
+//
+//  Created by kyungsoolee on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTestProject/GitTestProject/GitTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestProjectApp.swift
+//  GitTestProject
+//
+//  Created by kyungsoolee on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
