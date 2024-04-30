65ad4aa30630168d5ec2ed2f861916764a9e057a
dgh06175 <dgh06175@gmail.com>
2024-04-30 17:41:41 +0900

    feat: 에이스 뷰

---
+++ GitTestProject/GitTestProject/Ace/AceView.swift
@@ -0,0 +1,18 @@
+//
+//  AceView.swift
+//  GitTestProject
+//
+//  Created by 이상현 on 4/30/24.
+//
+
+import SwiftUI
+
+struct AceView: View {
+    var body: some View {
+        Text("Hello, Ace!")
+    }
+}
+
+#Preview {
+    AceView()
+}
